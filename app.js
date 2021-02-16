6838068baac4af9d1151e3f82205768aa3c241b2
FabioxUchiha <fcastellanos43@uan.edu.co>
2021-02-16 00:13:42 -0500

    agregando conexon con mongoose

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
+const mongodb = 'mongodb+srv://FabioxUchiha:Fbx12345*@cluster0.jw09m.mongodb.net/items-data?retryWrites=true&w=majority';
+
 app.set('view engine', 'ejs')
 
-app.listen(3000, ()=> {
-  console.log('Servidor escuchando')
+mongoose.connect(mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+.then(()=> {
+  console.log('conectado')
+  app.listen(3000, ()=> {
+    console.log('Servidor escuchando')
+  })
+})
+.catch((err)=>console.log(err))
+
 
 app.get('/', (req, res)=> {
-  res.render('index');
+  res.render('index',
+    {
+      title2: 'titulo index'
+    });
 })
 
 app.get('/contact', (req, res)=> {
