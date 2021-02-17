127c253ac12d111ff25b95cd0e3f5853a11f582c
Fabio Castellanos <fcastellanos43@uan.edu.co>
2021-02-17 18:00:55 -0500

    agregando vista de tabla para mostrar datos desde mongo en ejs

--- app.js
+++ app.js
@@ -1,73 +1,80 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const mongodb = 'mongodb+srv://FabioxUchiha:Fbx12345*@cluster0.jw09m.mongodb.net/items-data?retryWrites=true&w=majority';
-const Item = require('./models/item')
-app.use(express.urlencoded({
-  extended: true
-}))
-
-app.set('view engine', 'ejs')
-
-mongoose.connect(mongodb, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(()=> {
-  console.log('conectado')
-  app.listen(3000, ()=> {
-    console.log('Servidor escuchando')
+const mongoose = require("mongoose");
+const mongodb =
+  "mongodb+srv://FabioxUchiha:Fbx12345*@cluster0.jw09m.mongodb.net/items-data?retryWrites=true&w=majority";
+const Item = require("./models/item");
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-})
-.catch((err)=>console.log(err))
+);
 
+app.set("view engine", "ejs");
 
-app.get('/', (req, res)=> {
-  res.redirect('get-item')
-})
+mongoose
+  .connect(mongodb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("conectado");
+    app.listen(3000, () => {
+      console.log("Servidor escuchando");
+    });
+  })
+  .catch((err) => console.log(err));
 
-app.get('/contact', (req, res)=> {
-  res.render('contact');
-})
+app.get("/", (req, res) => {
+  res.redirect("get-item");
+});
 
-app.get('/add-item', (req, res)=> {
-  res.render('add-item');
-})
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
-app.get('/create-item', (req, res)=> {
+app.get("/add-item", (req, res) => {
+  res.render("add-item");
+});
+
+app.get("/create-item", (req, res) => {
   const item = new Item({
-    name: 'tostadora',
-    price: 120000
-  })
-  item.save()
-  .then(result => res.send(result))
-  .catch(err => res.send(err))
-})
+    name: "tostadora",
+    price: 120000,
+  });
+  item
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
+});
 
-app.get('/get-items', (req, res)=> {
+app.get("/get-items", (req, res) => {
   Item.find()
-  .then(result=>res.send(result))
-  .catch(err => res.send(err))
-})
+    .then((result) => {
+      res.render("get-items", { items: result });
+    })
+    .catch((err) => res.send(err));
+});
 
-app.post('/item', (req, res)=> {
+app.post("/item", (req, res) => {
   const item = new Item(req.body);
-  item.save()
-  .then(()=>{
-    res.redirect('/get-items')
-  })
-  .catch(err => res.send(err))
-})
+  item
+    .save()
+    .then(() => {
+      res.redirect("/get-items");
+    })
+    .catch((err) => res.send(err));
+});
 
+app.get("/get-item", (req, res) => {
+  Item.findById("602c2f1f9d44d746c986d235").then((result) =>
+    res.render("index", {
+      name: result.name,
+      price: result.price,
+    })
+  );
+});
 
-app.get('/get-item', (req, res)=> {
-  Item.findById('602c2f1f9d44d746c986d235')
-  .then(result=>res.render('index', {
-    name: result.name,
-    price: result.price,
-  }))
-})
-
-app.use((req, res)=> {
-  res.render('404');
-})
+app.use((req, res) => {
+  res.render("404");
+});
